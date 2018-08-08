d0e2e15f6fe82d995b6eb954daf4effd303f1919
kimbermenjivar <kimbermenjivar@gmail.com>
2018-08-08 14:00:18 -0400

    added get route for b1items

--- app/routes/bucketlist_routes.js
+++ app/routes/bucketlist_routes.js
@@ -12,12 +12,22 @@ const requireOwnership = customErrors.requireOwnership
 const requireToken = passport.authenticate('bearer', { session: false })
 const router = express.Router()
 
-router.get('/bl-items', (req, res) => {
+router.get('/bl-items/:id', (req, res) => {
 
 })
 
-router.get('/bl-items/:id', (req, res) => {
-
+router.get('/bl-items', (req, res) => {
+  blItem.find()
+    .then(blItems => {
+      // `examples` will be an array of Mongoose documents
+      // we want to convert each one to a POJO, so we use `.map` to
+      // apply `.toObject` to each one
+      return blItems.map(blItem => blItem.toObject())
+    })
+    // respond with status 200 and JSON of the examples
+    .then(blItems => res.status(200).json({ blItems: blItems }))
+    // if an error occurs, pass it to the handler
+    .catch(err => handle(err, res))
 })
 
 router.delete('/bl-items/:id', requireToken, (req, res) => {
