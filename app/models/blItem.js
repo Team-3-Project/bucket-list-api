a48b753e6099ec93473556a9cb1fac5e4416abf0
cymbiotica <terry.esposito@gmail.com>
2018-08-10 10:05:43 -0400

    fixed up model for blitems

--- app/models/blItem.js
+++ app/models/blItem.js
@@ -16,10 +16,6 @@ const blItemSchema = new mongoose.Schema({
   location: {
     type: String,
     required: true
-    name: {
-      type: String
-    }
-
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
@@ -38,4 +34,3 @@ const blItemSchema = new mongoose.Schema({
 
 
 module.exports = mongoose.model('BlItem', blItemSchema)
-
